18933b0dea676a8adfeb3a5c320b6cbb7b5c6275
sohampachpande <soham.pachpande@gmail.com>
2022-11-25 21:57:45 -0800

    bash scripts to create lambda/step functions

--- lambda-code-step/scripts/main.sh
+++ lambda-code-step/scripts/main.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 # stages=("scaledown" "crop" "mirrorx" "mirrory" "bw" "rotate" "watermark")
-stages=("crop" "mirrorx" "mirrory" "bw" "rotate" "watermark")
-version=withoutScaleDown_v1
+stages=("crop" "scaledown" "mirror" "mirror_bw" "mirror_2rotate" "mirror_3rotate")
+version=step-v2
 # Make Output Directory
 mkdir ./output/$version
 
 # Run Step Functions
 for stage in ${stages[@]}; do 
     echo $state
-    # aws lambda update-function-configuration --function-name pipeline-$stage-step --timeout 900 
+
+    ## CLI Command to Create a Lambda Function according to given Image URI
+    # aws lambda create-function --package-type Image --function-name pipeline-$stage-$version --role "arn:aws:iam::075165449331:role/BurnRate-BurnRateRole-65N0V6IDANLL" --memory-size 2048 --code ImageUri="075165449331.dkr.ecr.us-west-2.amazonaws.com/app-step:$stage-step-latest" --timeout 900 
+
+    ## CLI to Update Lambda Function Configuration
+    # aws lambda update-function-configuration --function-name pipeline-$stage-$version --timeout 900 
     source execute.sh $stage $version | tee ./output/$version/$stage.out 
 done
