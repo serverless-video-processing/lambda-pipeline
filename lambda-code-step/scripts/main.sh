30289eba8999794e352c6699a059bd808cfc44f6
sohampachpande <soham.pachpande@gmail.com>
2022-11-25 19:23:31 -0800

    new lambda power runs

--- lambda-code-step/scripts/main.sh
+++ lambda-code-step/scripts/main.sh
@@ -2,12 +2,13 @@
 
 # stages=("scaledown" "crop" "mirrorx" "mirrory" "bw" "rotate" "watermark")
 stages=("crop" "mirrorx" "mirrory" "bw" "rotate" "watermark")
-version=v5
+version=withoutScaleDown_v1
 # Make Output Directory
 mkdir ./output/$version
 
 # Run Step Functions
 for stage in ${stages[@]}; do 
+    echo $state
     # aws lambda update-function-configuration --function-name pipeline-$stage-step --timeout 900 
     source execute.sh $stage $version | tee ./output/$version/$stage.out 
 done
