3692b32a12a32a96debbef7e1a7cabf525f7a8d5
sohampachpande <soham.pachpande@gmail.com>
2022-11-25 17:24:46 -0800

    aws power tune output

--- lambda-code-step/scripts/main.sh
+++ lambda-code-step/scripts/main.sh
@@ -2,7 +2,12 @@
 
 # stages=("scaledown" "crop" "mirrorx" "mirrory" "bw" "rotate" "watermark")
 stages=("crop" "mirrorx" "mirrory" "bw" "rotate" "watermark")
+version=v5
+# Make Output Directory
+mkdir ./output/$version
 
+# Run Step Functions
 for stage in ${stages[@]}; do 
-    source execute.sh $stage v3 | tee $stage.out
+    # aws lambda update-function-configuration --function-name pipeline-$stage-step --timeout 900 
+    source execute.sh $stage $version | tee ./output/$version/$stage.out 
 done
