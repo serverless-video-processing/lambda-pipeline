070b167d3924938960c609d0814f43b49c8fa39b
sohampachpande <soham.pachpande@gmail.com>
2022-12-06 14:28:00 -0800

    exp data clean up

--- lambda-code-step/scripts/main.sh
+++ lambda-code-step/scripts/main.sh
@@ -5,8 +5,9 @@
 # op_count=(1 1 1 2 3 1)
 # stages=("crop" "scaledown" "mirror" "mirror_bw" "mirror_2rotate" "mirror_3rotate" )
 
-version=step-v3-512mb-memory
-stages=("crop" "scaledown" "mirror" "mirror_bw" "mirror_bw_rotate" "mirror_bw_rotate_watermark")
+lambda_version=step-v3
+step_version=step-agg1
+stages=("crop" "scaledown" "mirror_bw" "rotate_watermark")
 # stages=("mirror_bw_rotate" "mirror_bw_rotate_watermark")
 
 # Make Output Directory
@@ -18,10 +19,11 @@ for stage in ${stages[@]}; do
     echo $state
 
     ## CLI Command to Create a Lambda Function according to given Image URI
-    # version=step-v3
-    # aws lambda create-function --package-type Image --function-name pipeline-$stage-$version --role "arn:aws:iam::075165449331:role/BurnRate-BurnRateRole-65N0V6IDANLL" --memory-size 2048 --code ImageUri="075165449331.dkr.ecr.us-west-2.amazonaws.com/app-step:$stage-step-latest" --timeout 900 
+    # aws lambda create-function --package-type Image --function-name pipeline-$stage-$lambda_version --role "arn:aws:iam::075165449331:role/BurnRate-BurnRateRole-65N0V6IDANLL" --memory-size 2048 --code ImageUri="075165449331.dkr.ecr.us-west-2.amazonaws.com/app-step:$stage-step-latest" --timeout 900 
 
     ## CLI to Update Lambda Function Configuration
-    # aws lambda update-function-configuration --function-name pipeline-$stage-$version --timeout 900 
-    source execute.sh $stage $version | tee ./output/$version/$stage.out 
+    # aws lambda update-function-configuration --function-name pipeline-$stage-$lambda_version --timeout 900 
+
+    ## Run Step Function
+    # source execute.sh $stage $step_version | tee ./output/$step_version/$stage.out 
 done
