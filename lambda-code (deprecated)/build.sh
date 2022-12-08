af12f3939a36e2089b49ad2444e46a2d57a6774e
Gursharan Ahir <ahir.sharan@gmail.com>
2022-12-08 15:16:34 -0800

    restructure

--- lambda-code (deprecated)/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-stage=$1
-
-cd $stage
-docker build -t $stage-main .
-echo $stage-main:latest
-docker tag $stage-main:latest 075165449331.dkr.ecr.us-west-2.amazonaws.com/app:$stage-main-latest
-docker push 075165449331.dkr.ecr.us-west-2.amazonaws.com/app:$stage-main-latest
-cd ..
